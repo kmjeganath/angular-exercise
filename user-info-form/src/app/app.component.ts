3a1509cd52e5f400cf78dee920f66d2b8ec129ef
Jeganath <jeganath.km@thomsonreuters.com>
2019-11-05 19:45:21 +0530

    Basic form is created & basic validation is implemented

---
+++ user-info-form/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  userInfoForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.userInfoForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      firstName: ['', Validators.required],
+      mobileNumber: ['', Validators.required],
+      nationality: ['', Validators.required],
+      birthdate: ['', Validators.required],
+      address: ['', Validators.required],
+      addressDuration: ['', Validators.required],
+      maritalStatus: ['', Validators.required],
+    });
+  }
+
+  get getUserInfoForm() { return this.userInfoForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.userInfoForm.invalid) {
+      return;
+    }
+  }
+}
